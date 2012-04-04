6203d7a41fb800390fbf1e771fbb7e5a660a43e4
pjaspers <junkiesxl@gmail.com>
2012-04-04 16:57:56 +0200

    Removing plugin style

--- init.rb
+++
@@ -1,5 +0,0 @@
-# Include hook code here
-require "spreadsheet_on_rails"
-
-Mime::Type.register "application/excel", :xls
-ActionView::Template.register_template_handler 'rxls', SpreadsheetOnRails::Handler
